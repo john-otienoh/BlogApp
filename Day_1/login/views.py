231dfdb5cf070c312180472939861c2a6303090a
john-otienoh <johnteclaire@gmail.com>
2024-06-07 18:07:54 +0300

    create autbentication

--- Day_1/login/views.py
+++ Day_1/login/views.py
@@ -5,3 +5,7 @@ from django.contrib.auth.models import User, auth
 # Create your views here.
 def home(request):
     return render(request, 'login/base.html')
+
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
